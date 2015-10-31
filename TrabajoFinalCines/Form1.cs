fd1b43ea690837bbdef0b95b4db873ab25555a8c
diegoazh <diegoazh2003@gmail.com>
2015-10-31 15:55:28 -0300

    se corrigio el metodo de validación de usuarios logueados y se corrigio el script sql correspondiente.

--- TrabajoFinalCines/Form1.cs
+++ TrabajoFinalCines/Form1.cs
@@ -14,6 +14,7 @@ namespace TrabajoFinalCines
     {
         DatosSQL con;
         string database = "Data Source=.;Initial Catalog=CINES;Integrated Security=True";
+        string user, pass;
 
         public frmLogin()
         {
@@ -23,7 +24,36 @@ namespace TrabajoFinalCines
         private void frmLogin_Load(object sender, EventArgs e)
         {
             con = new DatosSQL(database);
-            con.conectar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // evaluamos los txt de usuario para ver cuales se rellenaron
+            if (txtAdministrador.Text != String.Empty && txtUsuario.Text == String.Empty)
+                user = txtAdministrador.Text;
+            else if (txtAdministrador.Text == String.Empty && txtUsuario.Text != String.Empty)
+                user = txtUsuario.Text;
+            else if (txtAdministrador.Text != String.Empty && txtUsuario.Text != String.Empty)
+            {
+                user = txtUsuario.Text;
+                txtAdministrador.Clear();
+            }
+
+            // evaluamos los txt de contraseña para ver cual esta rellenado
+            if (txtContrasenia2.Text != String.Empty && txtContrasenia.Text == String.Empty)
+                pass = txtContrasenia2.Text;
+            else if (txtContrasenia2.Text == String.Empty && txtContrasenia.Text != String.Empty)
+                pass = txtContrasenia.Text;
+            else if (txtContrasenia2.Text != String.Empty && txtContrasenia.Text != String.Empty)
+            {
+                pass = txtContrasenia.Text;
+                txtContrasenia2.Clear();
+            }
+
+            if (con.validarUsuario(user, pass))
+                MessageBox.Show("Logueado");
+            else
+                MessageBox.Show("Algo salio mal");
         }
     }
 }
